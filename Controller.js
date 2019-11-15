9c02263268619c5bc6c1ee58f3a3a7d977ef5047
GamGyul <zanghwkddnwl@naver.com>
2019-11-16 02:53:27 +0900

    zz

--- Controller.js
+++ Controller.js
@@ -1,15 +1,19 @@
-function load(path){
-	switch(path){
-		case "home":
-		case "program":
-			location.href="https://gamsoft.ga/";
-			break;
-		case "portfolio":
-			location.href="portfolio.html";
-			break;
-		default:
-			alert("잘못된 접근입니다.");
-			location.go(-1);
-			break;
-	}
+document.write(
+  "<script src='https://cdn.jsdelivr.net/npm/vue/dist/vue.js'></script>"
+);
+
+function load(path) {
+  switch (path) {
+    case "home":
+    case "program":
+      location.href = "https://gamsoft.ga/";
+      break;
+    case "portfolio":
+      location.href = "portfolio.html";
+      break;
+    default:
+      alert("잘못된 접근입니다.");
+      location.go(-1);
+      break;
+  }
 }
--- index.js
+++ index.js
@@ -1,91 +1,83 @@
+function goGoogle(e) {
+  link = $("#" + e.id).data("link");
 
-
-function goGoogle(e){
-	link = $("#"+e.id).data("link");
-
-	if(isMobile() && $("#"+e.id).data("type") == "mobile"){
-		location.href = link;
-	}else if(!isMobile() && $("#"+e.id).data("type") == "pc"){
-		location.href = link;
-	}else{
-		alert("해당되는 기기로 다운받아주세요.")
-	}
+  if (isMobile() && $("#" + e.id).data("type") == "mobile") {
+    location.href = link;
+  } else if (!isMobile() && $("#" + e.id).data("type") == "pc") {
+    location.href = link;
+  } else {
+    alert("해당되는 기기로 다운받아주세요.");
+  }
 }
 
-window.onload = function(){
-	setTimeout(function(){
-		scrollTo(0,0);
-	},100);
-}
+$(function() {
+  $("#homeBtn").click(function() {
+    load("home");
+  });
 
-$(function(){
+  $("#slide-menu ul li").click(function() {
+    var id = $(this).attr("id");
+    switch (id) {
+      case "sideMenuBtn1":
+        load("program");
+        break;
+      case "sideMenuBtn2":
+        load("portfolio");
+        break;
+      case "sideMenuBtn3":
+        load("none");
+        break;
+      case "sideMenuBtn4":
+        load("none");
+        break;
+      case "sideMenuBtn5":
+        load("none");
+        break;
+    }
+  });
 
-	$("#homeBtn").click(function(){
-		load('home');
-	});
+  function openMenu() {
+    $("#btnMenuText").html("닫기");
+    $("#menuIcon-m").hide();
+    $("#menuIcon-q").show();
+    $("#overlay").show();
+    $("#slide-menu").stop();
+    $("#slide-menu").animate(
+      {
+        left: 0
+      },
+      200,
+      function() {}
+    );
+  }
 
-	$("#slide-menu ul li").click(function(){
-		var id = $(this).attr('id');
-		
-		switch(id){
-			case 'sideMenuBtn1':
-			load('program');
-			break;
-			case 'sideMenuBtn2':
-			load('portfolio');
-			break;
-			case 'sideMenuBtn3':
-			load('none');
-			break;
-			case 'sideMenuBtn4':
-			load('none');
-			break;
-			case 'sideMenuBtn5':
-			load('none');
-			break;
-		}
+  function closeMenu() {
+    $("#btnMenuText").html("메뉴");
+    $("#menuIcon-m").show();
+    $("#menuIcon-q").hide();
+    $("#overlay").hide();
+    $("#slide-menu").stop();
+    $("#slide-menu").animate(
+      {
+        left: -200
+      },
+      200,
+      function() {}
+    );
+  }
 
+  $("#btnMenu").on("click", function() {
+    return (this.tog ^= 1) ? openMenu() : closeMenu();
+  });
 
-	});
-
-	function openMenu(){
-		$("#btnMenuText").html("닫기");
-		$("#menuIcon-m").hide();
-		$("#menuIcon-q").show();
-		$("#overlay").show();
-		$("#slide-menu").stop();
-		$("#slide-menu").animate({
-			'left': 0,
-		},200,
-		function() {
-
-		});
-	}
-
-	function closeMenu(){
-		$("#btnMenuText").html("메뉴");
-		$("#menuIcon-m").show();
-		$("#menuIcon-q").hide();
-		$("#overlay").hide();
-		$("#slide-menu").stop();
-		$("#slide-menu").animate({
-			'left': -200,
-		},200,
-		function() {
-
-		});
-	}
-
-	$("#btnMenu").on("click", function () {
-		return (this.tog ^= 1) ? openMenu() : closeMenu();
-	});
-
-	$("#overlay").on("click", function () {
-		closeMenu();
-		$("#btnMenu")[0].tog = 0;
-	});
+  $("#overlay").on("click", function() {
+    closeMenu();
+    $("#btnMenu")[0].tog = 0;
+  });
 });
 
 function isMobile() {
-	return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
 }
